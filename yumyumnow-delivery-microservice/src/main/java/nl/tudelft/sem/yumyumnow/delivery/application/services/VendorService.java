4b5c069613e2c603a4f24af7e962c30dbcaaa306
Alexandra Mihaela Nicola <A.M.Nicola@student.tudelft.nl>
2023-12-17 14:02:31 +0200

    Removed customer and vendor repositories.

--- yumyumnow-delivery-microservice/src/main/java/nl/tudelft/sem/yumyumnow/delivery/application/services/VendorService.java
+++ yumyumnow-delivery-microservice/src/main/java/nl/tudelft/sem/yumyumnow/delivery/application/services/VendorService.java
@@ -1,20 +1,9 @@
 package nl.tudelft.sem.yumyumnow.delivery.application.services;
 
-import nl.tudelft.sem.yumyumnow.delivery.domain.repos.VendorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VendorService {
-    private final VendorRepository vendorRepository;
 
-    /**
-     * Create a new VendorService.
-     *
-     * @param vendorRepository The repository to use.
-     */
-    @Autowired
-    public VendorService(VendorRepository vendorRepository){
-        this.vendorRepository = vendorRepository;
-    }
 }
