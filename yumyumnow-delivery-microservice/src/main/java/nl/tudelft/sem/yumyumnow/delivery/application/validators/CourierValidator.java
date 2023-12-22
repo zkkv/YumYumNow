c5f56c172d04055eafa02bcf532e785a0a79249a
Rafayel Gardishyan <rgardishyan@gmail.com>
2023-12-22 13:38:29 +0100

    Commented out the validator that breaks the build

--- yumyumnow-delivery-microservice/src/main/java/nl/tudelft/sem/yumyumnow/delivery/application/validators/CourierValidator.java
+++ yumyumnow-delivery-microservice/src/main/java/nl/tudelft/sem/yumyumnow/delivery/application/validators/CourierValidator.java
@@ -1,11 +1,11 @@
-package nl.tudelft.sem.yumyumnow.delivery.application.validators;
-
-import nl.tudelft.sem.yumyumnow.delivery.domain.model.dto.Vendor;
-import nl.tudelft.sem.yumyumnow.delivery.model.Delivery;
-
-public class CourierValidator extends AuthProcessor<Courier> {
-    @Override
-    public boolean process(Courier toValidate, Delivery delivery) {
-        return false;
-    }
-}
+//package nl.tudelft.sem.yumyumnow.delivery.application.validators;
+//
+//import nl.tudelft.sem.yumyumnow.delivery.domain.model.dto.Vendor;
+//import nl.tudelft.sem.yumyumnow.delivery.model.Delivery;
+//
+//public class CourierValidator extends AuthProcessor<Courier> {
+//    @Override
+//    public boolean process(Courier toValidate, Delivery delivery) {
+//        return false;
+//    }
+//}
