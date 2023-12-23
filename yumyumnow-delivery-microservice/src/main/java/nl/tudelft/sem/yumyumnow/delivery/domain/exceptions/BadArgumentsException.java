8437d1eb1920a078e0f99f1d47d70ef45c48f4c6
Kirill Zhankov <k.zhankov@student.tudelft.nl>
2023-12-23 23:59:47 +0100

    Rename BadArgumentsException to BadArgumentException

--- yumyumnow-delivery-microservice/src/main/java/nl/tudelft/sem/yumyumnow/delivery/domain/exceptions/BadArgumentsException.java
+++
@@ -1,7 +0,0 @@
-package nl.tudelft.sem.yumyumnow.delivery.domain.exceptions;
-
-public class BadArgumentsException extends Exception {
-    public BadArgumentsException(String errorMessage) {
-        super(errorMessage);
-    }
-}
