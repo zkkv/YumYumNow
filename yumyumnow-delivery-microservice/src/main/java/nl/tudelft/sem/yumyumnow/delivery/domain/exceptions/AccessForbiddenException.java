b25835e98935046da9de04827ab7d4aecd876d84
Kirill Zhankov <k.zhankov@student.tudelft.nl>
2023-12-24 17:20:35 +0100

    Add descriptions to exception classes

--- yumyumnow-delivery-microservice/src/main/java/nl/tudelft/sem/yumyumnow/delivery/domain/exceptions/AccessForbiddenException.java
+++ yumyumnow-delivery-microservice/src/main/java/nl/tudelft/sem/yumyumnow/delivery/domain/exceptions/AccessForbiddenException.java
@@ -1,5 +1,8 @@
 package nl.tudelft.sem.yumyumnow.delivery.domain.exceptions;
 
+/**
+ * Exception to be thrown when the given user does not have rights to use an endpoint.
+ */
 public class AccessForbiddenException extends Exception {
     public AccessForbiddenException(String errorMessage) {
         super(errorMessage);
--- yumyumnow-delivery-microservice/src/main/java/nl/tudelft/sem/yumyumnow/delivery/domain/exceptions/NoDeliveryFoundException.java
+++ yumyumnow-delivery-microservice/src/main/java/nl/tudelft/sem/yumyumnow/delivery/domain/exceptions/NoDeliveryFoundException.java
@@ -1,5 +1,8 @@
 package nl.tudelft.sem.yumyumnow.delivery.domain.exceptions;
 
+/**
+ * Exception to be thrown when the delivery with the given id cannot be found.
+ */
 public class NoDeliveryFoundException extends Exception {
     public NoDeliveryFoundException(String errorMessage) {
         super(errorMessage);
