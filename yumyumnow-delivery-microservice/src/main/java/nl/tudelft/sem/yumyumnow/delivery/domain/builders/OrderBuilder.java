c4ce99f5096408e2f55f2c4493be08f7c8338098
esmura <esmura@student.tudelft.nl>
2024-01-10 12:43:21 +0100

    added javadoc to the methods in the new OrderBuilder class

--- yumyumnow-delivery-microservice/src/main/java/nl/tudelft/sem/yumyumnow/delivery/domain/builders/OrderBuilder.java
+++ yumyumnow-delivery-microservice/src/main/java/nl/tudelft/sem/yumyumnow/delivery/domain/builders/OrderBuilder.java
@@ -1,5 +1,7 @@
 package nl.tudelft.sem.yumyumnow.delivery.domain.builders;
 import java.util.UUID;
+
+import nl.tudelft.sem.yumyumnow.delivery.domain.dto.Order;
 import nl.tudelft.sem.yumyumnow.delivery.domain.dto.Vendor;
 import nl.tudelft.sem.yumyumnow.delivery.domain.dto.Customer;
 public class OrderBuilder {
@@ -7,19 +9,41 @@ public class OrderBuilder {
     private Vendor vendor;
     private Customer customer;
 
+    /**
+     * A setter for the Order id of the OrderBuilder
+     * @param id the UUID to be set as the order id
+     * @return the instance of the OrderBuilder that it was called on, with the id field updated
+     */
     public OrderBuilder setOrderId(UUID id) {
         this.id = id;
         return this;
     }
 
+    /**
+     * A setter for the Order vendor of the OrderBuilder
+     * @param vendor the Vendor to be set as the vendor for this order
+     * @return the instance of the OrderBuilder that it was called on, with the vendor field updated
+     */
     public OrderBuilder setOrderVendor(Vendor vendor) {
         this.vendor = vendor;
         return this;
     }
 
+    /**
+     * A setter for the Order customer of the OrderBuilder
+     * @param customer the Customer to be set as the customer for this order
+     * @return the instance of the OrderBuilder that it was called on, with the customer field updated
+     */
     public OrderBuilder setOrderCustomer(Customer customer) {
         this.customer = customer;
         return this;
     }
 
+    /**
+     * a method that creates a new Order object using the field of the OrderBuilder
+     * @return the Order with the same fields as the OrderBuilder
+     */
+    public Order createOrder() {
+        return new Order(id, vendor, customer);
+    }
 }
