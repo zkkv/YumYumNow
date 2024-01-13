d2f2de126228149ba890a2aa634b60dcf23aca46
Horia Radu <radu-1@student.tudelft.nl>
2024-01-13 17:55:50 +0100

    remove some unused imports

--- yumyumnow-delivery-microservice/src/main/java/nl/tudelft/sem/yumyumnow/delivery/application/services/EmailService.java
+++ yumyumnow-delivery-microservice/src/main/java/nl/tudelft/sem/yumyumnow/delivery/application/services/EmailService.java
@@ -1,13 +1,5 @@
 package nl.tudelft.sem.yumyumnow.delivery.application.services;
 
-import nl.tudelft.sem.yumyumnow.delivery.domain.dto.Customer;
-import nl.tudelft.sem.yumyumnow.delivery.domain.dto.Order;
-import nl.tudelft.sem.yumyumnow.delivery.domain.exceptions.BadArgumentException;
-import nl.tudelft.sem.yumyumnow.delivery.domain.repos.DeliveryRepository;
-import nl.tudelft.sem.yumyumnow.delivery.model.Delivery;
-import nl.tudelft.sem.yumyumnow.delivery.model.DeliveryIdStatusPutRequest;
-
-import java.util.UUID;
 
 public class EmailService {
 
