c44f2eed2a6d6704fecb979034969b5327f5215e
esmura <esmura@student.tudelft.nl>
2024-01-18 09:11:45 +0100

    started a test class

---
+++ yumyumnow-delivery-microservice/src/test/java/nl/tudelft/sem/yumyumnow/delivery/application/validators/UserIdAdminValidatorTest.java
@@ -0,0 +1,10 @@
+package nl.tudelft.sem.yumyumnow.delivery.application.validators;
+
+import org.junit.jupiter.api.Test;
+
+public class UserIdAdminValidatorTest {
+    @Test
+    void process() {
+
+    }
+}
